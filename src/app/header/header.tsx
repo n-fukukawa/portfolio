6e852fde2d1155a1d5e684c4e8116c21c81c198a
Naruki Fukukawa <alnair9501@gmail.com>
2024-02-12 23:00:52 +0900

    githubへのアクセス

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import Navigation from './navigation'
-import { IconButton } from '@mui/material'
 import { GitHub } from '@mui/icons-material'
 
 export default function Header() {
@@ -10,15 +9,15 @@ export default function Header() {
       className="fixed w-full flex items-center sm:justify-start justify-between sm:px-16 px-4 py-4 border-b-2 border-b-slate-200 bg-white"
       style={{ zIndex: 1, height: 90 }}
     >
-      <div className="mr-16 p-2">
+      <div className="flex items-center mr-16 p-2">
         <Link href="/" className="text-3xl">
           fukulab
         </Link>{' '}
       </div>
       <Navigation />
-      <div>
-        <Link href="" target="_blank">
-          <GitHub />
+      <div className="sm:block hidden grow text-right hover:text-sky-800">
+        <Link href="https://github.com/n-fukukawa/portfolio" target="_blank">
+          <GitHub fontSize="large" />
         </Link>
       </div>
     </header>
